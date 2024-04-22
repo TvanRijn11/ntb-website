33cc860c3c750155fbfac51dce5fe8fc62a7066e
tomvr11 <t.vanrijn@student.utwente.nl>
2024-04-22 12:03:54 +0200

    start home

---
+++ src/app/shared/components/text-picture/text-picture.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-picture',
+  templateUrl: './text-picture.component.html',
+  styleUrl: './text-picture.component.scss'
+})
+export class TextPictureComponent {
+  @Input() layout: number = 1;
+  @Input() picture: string = 'empty.png';
+  @Input() title: string = '';
+  @Input() text: string = '';
+
+  picUrl!: string;
+
+  OnInit() {
+    this.picUrl = '../../../../assets/' + this.picture
+  }
+
+}
